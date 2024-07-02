37b348b5bb73e3ce0acce2f9c1ad4bf62e9fb2ab
enzogit <enzo_91_10@hotmail.com>
2024-07-02 19:59:51 -0300

    winston casi correcto loggers corregido

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,55 +1,39 @@
-import { configDotenv } from 'dotenv'
-import winston, { transports } from 'winston'
-const {node_env} =configDotenv
+import winston from "winston";
+import { config } from "dotenv";
 
 const niveles = {
-    nivel: {
-        fatal: 0,
-        error: 1,
-        warning: 2,
-        info: 3,
-        http: 4,
-        debug: 5
-    },
-    colors: {
-        fatal: "red",
-        error: "yellow",
-        warning: "blue",
-        info: "green",
-        http: "magenta",
-        debug: "white"
-    }
+    fatal: 0, 
+    error: 1, 
+    warning: 2,
+    info: 3, 
+    http: 4, 
+    debug: 5
 }
+
 const loggerDevelopment = winston.createLogger({
-    levels: niveles,
+    levels: niveles, 
     transports: [
         new winston.transports.Console({
             level: "debug"
         })
     ]
 })
+
 const loggerProduction = winston.createLogger({
-    levels: niveles,
+    levels: niveles, 
     transports: [
-        new winston.transports.Console({
-            level: "http",
-            format: winston.format.combine(
-                winston.format.colorize({ colors: niveles.colors }),
-                winston.format.simple()
-            )
-        }),
         new winston.transports.File({
-            filename: "./errors.log",
+            filename: "./errors.log", 
             level: "error"
         })
-
     ]
 })
-const logger = node_env === "production" ? loggerProduction: loggerDevelopment;
+
+const logger = config === "production" ? loggerProduction : loggerDevelopment; 
 
 const addLogger = (req, res, next) => {
-    req.logger = logger;
-    req.logger.http(`${req.method} en ${req.url} - ${new Date().toLocaleTimeString()}`)
-    next()
+    req.logger = logger; 
+    req.logger.http(`${req.method} en ${req.url} - ${new Date().toLocaleTimeString()}`);
+    next();
 }
-export default addLogger
+export default addLogger;
